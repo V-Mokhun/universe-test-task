4680d9ec95369250ee71556d775ddf290907a282
v-mokhun <v.mokhun@gmail.com>
2025-08-22 10:46:34 +0200

    feat: install pagination component and setup react-query

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -9,31 +9,36 @@ import { ProtectedRoute } from "./shared/components/protected-route";
 import { Layout } from "./shared/components/layout";
 import { LoginPage, RegisterPage } from "./pages";
 import { ProjectsPage } from "./pages/projects";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Layout>
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <Router>
+          <Layout>
+            <Routes>
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/register" element={<RegisterPage />} />
 
-            <Route
-              path="/projects"
-              element={
-                <ProtectedRoute>
-                  <ProjectsPage />
-                </ProtectedRoute>
-              }
-            />
+              <Route
+                path="/projects"
+                element={
+                  <ProtectedRoute>
+                    <ProjectsPage />
+                  </ProtectedRoute>
+                }
+              />
 
-            <Route path="/" element={<Navigate to="/projects" replace />} />
+              <Route path="/" element={<Navigate to="/projects" replace />} />
 
-            <Route path="*" element={<Navigate to="/projects" replace />} />
-          </Routes>
-        </Layout>
-      </Router>
-    </AuthProvider>
+              <Route path="*" element={<Navigate to="/projects" replace />} />
+            </Routes>
+          </Layout>
+        </Router>
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
